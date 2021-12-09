727310c22f8f01ed4a4fd3f09478c98d70a1eb48
andrey Nemodruk <nemodruk.briz.ua>
2021-12-09 23:23:29 +0200

    fix routes

--- src/middlewares/routes/healthRoute.js
+++
@@ -1,8 +0,0 @@
-const Router = require('@koa/router');
-
-const router = new Router();
-const subscriptionsHealth = require('../../controllers/subscriptionsHealth');
-
-router.get('/subscriptions/health', subscriptionsHealth);
-
-module.exports = router;
--- src/middlewares/routes/routes.js
+++ src/middlewares/routes/routes.js
@@ -1,6 +1,18 @@
 const combineRouters = require('koa-combine-routers');
-const healthRoute = require('./healthRoute');
-const subscriptionsRoute = require('./subscriptionsRoute');
+const Router = require('@koa/router');
+const subscriptionsHealth = require('../../controllers/subscriptionsHealth');
+const createSubscription = require('../../controllers/createSubscription');
+const getSubscription = require('../../controllers/getSubscription');
+const deleteSubscriptions = require('../../controllers/deleteSubscriptions');
+
+const healthRoute = new Router();
+const subscriptionsRoute = new Router();
+
+healthRoute.get('/subscriptions/health', subscriptionsHealth);
+
+subscriptionsRoute.post('/subscriptions', createSubscription);
+subscriptionsRoute.get('/subscriptions', getSubscription);
+subscriptionsRoute.delete('/subscriptions', deleteSubscriptions);
 
 const router = combineRouters(healthRoute, subscriptionsRoute);
 
